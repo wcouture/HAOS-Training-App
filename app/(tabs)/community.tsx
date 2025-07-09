03f22fb85fba0be56254154ba78933611a4de2e0
William Couture <wcouture17@gmail.com>
2025-07-09 11:30:41 -0400

    Displays supplement and community page in fullscreen webviews

--- app/(tabs)/community.tsx
+++ app/(tabs)/community.tsx
@@ -1,11 +1,14 @@
-import { Text } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import WebView from "react-native-webview";
 
 export default function Community() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Community</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <WebView
+          style={{ flex: 1 }}
+          source={{ uri: "https://www.facebook.com/share/g/1F3i1VTd64/" }}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- app/(tabs)/supplements.tsx
+++ app/(tabs)/supplements.tsx
@@ -1,11 +1,24 @@
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { useEffect } from "react";
+import { Linking } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import WebView from "react-native-webview";
 
 export default function Supplements() {
+  useEffect(() => {
+    const openSupplementsSite = async () => {
+      await Linking.openURL("https://bldrsports.com/");
+      router.replace("/");
+    };
+    //openSupplementsSite();
+  });
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Supplements</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <WebView
+          style={{ flex: 1 }}
+          source={{ uri: "https://bldrsports.com/" }}
+        />
       </SafeAreaView>
     </SafeAreaProvider>
   );
